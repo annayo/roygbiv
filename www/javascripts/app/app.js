8a095855fe828811e3454986ca83a34af7f10db5
Anna Yovandich <ayovandich@cpbgroup.com>
2013-04-01 01:29:04 -0700

    added height toggle .cta, .reveal rule, and transition mixin

--- www/javascripts/app/app.js
+++ www/javascripts/app/app.js
@@ -5,10 +5,26 @@ define(['jquery'], function($) {
 
 	'use strict';
 
-	return {
+	var self = {
 		'initialize': function () {
-			// start application
+
+			// append button to avoid non-semantic dom clutter
+			self.$cta = $('<span />', { 'class': 'cta', 'text': 'double your fun!', 'data-toggle-text': 'less fun' })
+						.appendTo($('#main'))
+						.on('click', self.toggle);
+		},
+
+		'toggle': function () {
+
+			// store current button text 
+			var currText = self.$cta.text();
+			// toggle button text and button's data-toggle-text values
+			self.$cta.text(self.$cta.data('toggle-text')).data('toggle-text', currText);
+			
+			$('.rainbow').toggleClass('reveal');
 		}
 	}
+
+	return self;
 
 });
